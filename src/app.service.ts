4d88f8d96253527f557a4b0c4daeeb905b418a71
Raul Aquino <aquinoraul207@gmail.com>
2024-04-19 11:25:26 -0300

    [main] tests regarding the configuration update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 import { SensorInstancesService } from 'src/sensors/sensor-instances.service';
 
 @Injectable()
@@ -8,6 +9,8 @@ export class AppService {
     private readonly sensorInstancesService: SensorInstancesService
   ) { }
 
+  private cronJobsMap: Map<any, CronJob> = new Map();
+
   @Cron('* * * * * *')
   async handleCron() {
     const instances = this.sensorInstancesService.getAllInstances();
@@ -15,6 +18,12 @@ export class AppService {
     for (const instance of instances) {
       if ('syncData' in instance) {
         instance.syncData({});
+      }
+
+      if (instance.updatedConfigs && !this.cronJobsMap.has(instance)) {
+        const job = new CronJob('*/5 * * * * *', instance.updatedConfigs.bind(instance));
+        job.start();
+        this.cronJobsMap.set(instance, job);
       }
     }
   }
--- src/sensors/blue-sensor.service.ts
+++ src/sensors/blue-sensor.service.ts
@@ -1,4 +1,5 @@
 import { Logger } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 
 export class BlueSensorService implements APISensorConsumer<any>, SensorLayer<any>{
 
@@ -10,13 +11,19 @@ export class BlueSensorService implements APISensorConsumer<any>, SensorLayer<an
 
     lastSyncTime: number = 0;
     syncInterval: number = 15000; // 15 seconds
+    configurationVariable: number = 0;
+
+    async updatedConfigs() {
+        this.configurationVariable += 1;
+        this.logger.log(`⚙️ The sensor 🟦 configuration has been updated ⚙️ ${this.configurationVariable}`);
+    }
 
     syncData(data: any): void { 
         const currentTime = new Date().getTime();
         const elapsedTime = currentTime - this.lastSyncTime;
 
         if (elapsedTime >= this.syncInterval) {
-            this.logger.log('🟦🟦 BlueSensorService syncing data 🟦🟦');
+            this.logger.log(`🟦🟦 BlueSensorService syncing data 🟦🟦 with the configuration ⚙️ ${this.configurationVariable} ⚙️`);
             this.logger.log(`🟦🟦 synchronization time: ${this.syncInterval} 🟦🟦`);
             this.processData(data);
             this.lastSyncTime = currentTime;
